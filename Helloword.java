069d410164fe0f6a47f535028c28bc138152c1bb
josethomas45 <145546568+josethomas45@users.noreply.github.com>
2023-09-26 14:58:54 +0530

    helloworld

---
+++ Helloword.java
@@ -0,0 +1,17 @@
+/**********************************************************
+ * File        :  Helloworld.java
+ * Description :  Just a sample
+ * Author      :  Jose Thomas
+ * Version     :  1.0
+ * Date        :  26/09/2023
+ **********************************************************/
+package javaproj1;
+
+public class Helloword {
+
+	public static void main(String[] args) {
+	
+	System.out.println("Hello world");
+	
+	}
+}
