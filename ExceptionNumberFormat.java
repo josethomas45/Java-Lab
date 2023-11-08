f6a18039a5e67497921cb9def6051e0db07301c2
Jose Thomas <145546568+josethomas45@users.noreply.github.com>
2023-11-08 09:38:28 +0530

    Add files via upload

---
+++ ExceptionNumberFormat.java
@@ -0,0 +1,31 @@
+
+public class ExceptionNumberFormat {
+	
+public static void main(String[] args) {
+		
+		try {
+			String s="abc";
+			int [] array= new int[5];
+			System.out.println(Integer.parseInt(s));
+			System.out.println(10/0);
+			System.out.println(array[6]);
+		}
+		
+		catch(NumberFormatException e){
+			System.out.println("Sorry, number format");
+		}
+		
+		catch(ArithmeticException e){
+			System.out.println("Sorry, Arithematic exception");
+		}
+		
+		catch(Exception e){
+			System.out.println("Unexpected exception occured");
+		}
+		
+		finally {
+			System.out.println("program runs");
+			}
+
+}
+}
